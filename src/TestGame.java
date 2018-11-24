d55ee8ad94863308902ea39300d91d42e4ad820c
jeffery1999 <45302420+jeffery1999@users.noreply.github.com>
2018-11-24 13:00:25 +0800

    jia hao

--- src/TestGame.java
+++ src/TestGame.java
@@ -5,6 +5,40 @@ public class TestGame {
 		
 		List<Integer>cardNumber = new ArrayList<>();
 		
+		Pokemon [] atk_pokemon = new Pokemon[11];
+		Pokemon pokemon1 = new Pokemon("Blaziken",296,3,140,0,"red","Attacking","Blaze Kick","Active");
+		Pokemon pokemon2 = new Pokemon("Charizard",200,3,96,0,"red","Attacking","Fire Blast","Active");
+		Pokemon pokemon3 = new Pokemon("Charmander",50,1,24,0,"red","Attacking","Flamethrower","Active");
+		Pokemon pokemon4 = new Pokemon("Charmeleon",100,2,48,0,"red","Attacking","Fire Wheel","Active");
+		Pokemon pokemon5 = new Pokemon("Combusken",148,2,70,0,"red","Attacking","Flame Charge","Active");
+		Pokemon pokemon6 = new Pokemon("Primal Kyogre",50,1,40,0,"blue","Attacking","Judgement Storm","Active");
+		Pokemon pokemon7 = new Pokemon("Primal Kyogre2",100,2,80,0,"blue","Attacking","Wind Walker Remorse","Active");
+		Pokemon pokemon8 = new Pokemon("Pichu",56,1,37,0,"yellow","Attacking","Sparks","Active");
+		Pokemon pokemon9 = new Pokemon("Pikachu",112,2,74,0,"yellow","Attacking","Lighthing Ball","Active");
+		Pokemon pokemon10 = new Pokemon("Raichu",224,3,148,0,"yellow","Attacking","Ray Bolt","Active");
+		Pokemon pokemon11 = new Pokemon("Torchic",74,1,35,0,"red","Attacking","Ember","Active");
+		Pokemon pokemon12 = new Pokemon("Bastiodon",140,2,92,0,"yellow","Defending","Iron Body","Active");
+		Pokemon pokemon13 = new Pokemon("Blastoise",240,3,42,0,"blue","Defending","Block","Active");
+		Pokemon pokemon14 = new Pokemon("Eleckid",70,1,20,0,"yellow","Defending","Electric Barrier","Active");
+		Pokemon pokemon15 = new Pokemon("Electabuzz",140,2,40,0,"yellow","Defending","Static Barier","Active");
+		Pokemon pokemon16 = new Pokemon("Electivire",280,3,80,0,"yellow","Defending","Raijin Barrier","Active");
+		Pokemon pokemon17 = new Pokemon("Shieldon",70,1,46,0,"yellow","Defending","Iron head","Active");
+		Pokemon pokemon18 = new Pokemon("Slowbro",130,2,76,0,"blue","Defending","Sleep","Active");
+		Pokemon pokemon19 = new Pokemon("Slowking",260,3,152,0,"blue","Defending","Hibernate","Active");
+		Pokemon pokemon20 = new Pokemon("Slowpoke",65,1,38,0,"blue","Defending","Nap","Active");
+		Pokemon pokemon21 = new Pokemon("Squirtle",30,1,21,0,"blue","Defending","Defense Curl","Active");
+		Pokemon pokemon22 = new Pokemon("Wartortle",120,2,42,0,"blue","Defending","Iron Defense","Active");
+		Pokemon pokemon23 = new Pokemon("Gardevoir",200,3,160,0,"colourless","Fairy","Entice","Active");
+		Pokemon pokemon24 = new Pokemon("Igglybuff",50,1,42,0,"colourless","Fairy","Lullaby","Active");
+		Pokemon pokemon25 = new Pokemon("Jigglypuff",100,2,84,0,"colourless","Fairy","Confusion Sense","Active");
+		Pokemon pokemon26 = new Pokemon("Kirlia",100,2,80,0,"colourless","Fairy","Confusion","Active");
+		Pokemon pokemon27 = new Pokemon("Kricketot",54,1,42,0,"red","Fairy","Sing","Active");
+		Pokemon pokemon28 = new Pokemon("Kricketune",108,2,84,0,"red","Fairy","Musical Performance","Active");
+		Pokemon pokemon29 = new Pokemon("Ninetales",110,2,80,0,"red","Fairy","Fire Trap","Active");
+		Pokemon pokemon30 = new Pokemon("Ralts",50,1,40,0,"colourless","Fairy","Telepathy","Active");
+		Pokemon pokemon31 = new Pokemon("Vulpix",55,1,40,0,"red","Fairy","Fire Spin","Active");
+		Pokemon pokemon32 = new Pokemon("Wigglypuff",150,3,168,0,"colourless","Fairy","Mind Blast","Active");
 		
+	
 	}
 }
