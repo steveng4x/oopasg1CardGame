8c25f079b74b7e5f23b3c113069b3983e1c66bf0
Frediho0116 <45302405+Frediho0116@users.noreply.github.com>
2018-11-24 12:58:28 +0800

    table

---
+++ src/oopasg1CardGame/player1cards.java
@@ -0,0 +1,31 @@
+package oopasg1CardGame;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JFrame;
+
+public class player1cards extends JTable {
+	public String columnheader;
+	public String player_1_cards;
+	JTable Player1;
+	
+	String[] column_header= {"No.","Type","Stage","Experience","Hit Point","Energy","Energy Colour","Attack Point","Resistance Point","Status"};
+	String[][] player1cards= {{"1","Defending","0","0","55","30","Red","-","3","Active"}};
+	
+	public player1cards()
+	{ 
+		Player1=new JTable();
+		Player1.setBounds(50,50,200,230);
+		JScrollPane js=new JScrollPane(Player1);
+		this.add(Player1);
+		this.setSize(300,400);
+		this.setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new player1cards();
+	}
+
+}
