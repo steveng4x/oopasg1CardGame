fb9b5eaf0f3a10a888fc8b263c4731c422860ed1
steveng4x <steveng4x@users.noreply.github.com>
2018-11-25 15:49:48 +0800

    asd

--- src/Board.java
+++ src/Board.java
@@ -11,16 +11,7 @@ public class Board extends Turn{
 
 	
 	public void pokeList() {
-		List<Integer>Number = new ArrayList<>();
-		List<String>Type = new ArrayList<>();
-		List<Integer>Stage = new ArrayList<>();
-		List<Integer>Experience = new ArrayList<>();
-		List<Integer>HitPoint = new ArrayList<>();
-		List<Integer>Energy = new ArrayList<>();
-		List<String>EnergyColour = new ArrayList<>();
-		List<Integer>AttackPoint = new ArrayList<>();
-		List<Integer>ResistancePoint = new ArrayList<>();
-		List<String>Status = new ArrayList<>();
+
 	}
 	
 	public int getTurn() {
